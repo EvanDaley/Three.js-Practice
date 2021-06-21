5e2f747c80c338741f2fcc57d46be4f20920b4d5
Evan Daley <evandaley@Evans-MacBook-Pro.local>
2021-06-20 21:22:15 -0700

    Adding a cube for every named color

--- ThreeJsBook/5_Transformations/src/World/components/cube.js
+++ ThreeJsBook/5_Transformations/src/World/components/cube.js
@@ -2,35 +2,36 @@ import {
   BoxBufferGeometry,
   MathUtils,
   Mesh,
-  MeshStandardMaterial,
+  MeshBasicMaterial,
 } from 'https://unpkg.com/three@0.127.0/build/three.module.js';
 
-function createCube() {
+function createCube(color, position) {
   const geometry = new BoxBufferGeometry(2, 2, 2);
 
-  const material = new MeshStandardMaterial({ color: 'purple' });
+  const material = new MeshBasicMaterial({ color: color });
 
   const cube = new Mesh(geometry, material);
 
-  cube.position.x = -0.5;
-  cube.position.y = -0.1;
-  cube.position.z = 1;
+  cube.position.x = position.x
+  cube.position.y = position.y
+  cube.position.z = position.z
 
   // equivalent to:
   // cube.position.set(-0.5, -0.1, 1);
 
-  cube.scale.x = 1.25;
-  cube.scale.y = 0.25;
-  cube.scale.z = 0.5;
+  cube.scale.x = .1
+  cube.scale.y = .1
+  cube.scale.z = .02
 
   // equivalent to:
   // cube.scale.set(1.25, 0.25, 0.5);
 
   // to rotate using degrees, they must
   // first be converted to radians
-  cube.rotation.x = MathUtils.degToRad(-60);
-  cube.rotation.y = MathUtils.degToRad(-45);
-  cube.rotation.z = MathUtils.degToRad(60);
+  
+  // cube.rotation.x = MathUtils.degToRad(-2);
+  // cube.rotation.y = MathUtils.degToRad(-2);
+  // cube.rotation.z = MathUtils.degToRad(6);
 
   return cube;
 }
---
+++ ThreeJsBook/README.md
@@ -0,0 +1,5 @@
+This repo contains a bunch of little practice projects that I've been working on to get a better feel for three-js. 
+
+A lot of this was guided by discoverthreejs.com and threejs-journey.xyz
+
+Big shout out to @looeee on github and Bruno Simon for their incredible training content.
