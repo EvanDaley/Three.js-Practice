1e5e7cf24f6b54e74ae48854fb8ff99f7aabc67d
Evan Daley <evandaley@Evans-MacBook-Pro.local>
2021-06-24 18:51:49 -0700

    Adding another scene. Learning camera controls. Making the cube spin madly.

---
+++ ThreeJsBook/8_CameraControls/src/World/World.js
@@ -0,0 +1,68 @@
+import { createCamera } from './components/camera.js';
+import { createCube } from './components/cube.js';
+import { createLights } from './components/lights.js';
+import { createScene } from './components/scene.js';
+
+import { createRenderer } from './systems/renderer.js';
+import { Resizer } from './systems/Resizer.js';
+import { Loop } from './systems/Loop.js';
+import { createControls } from './systems/controls.js';
+
+let camera;
+let renderer;
+let scene;
+let loop;
+
+class World {
+  constructor(container) {
+    camera = createCamera();
+    renderer = createRenderer();
+    scene = createScene();
+    loop = new Loop(camera, scene, renderer);
+    container.append(renderer.domElement);
+
+    const controls = createControls(camera, renderer.domElement);
+
+    const cube = createCube();
+    const light = createLights();
+
+    controls.target.copy(cube.position);
+    controls.enablePan = true
+    controls.enableDamping = true;
+    controls.enableKeys = true;
+
+    controls.autoRotate = true;
+    controls.autoRotateSpeed = 1000;
+
+    controls.minDistance = 5;
+    controls.maxDistance = 20;
+
+    controls.minAzimuthAngle = - Infinity; // default
+    controls.maxAzimuthAngle = Infinity; // default
+
+    controls.minPolarAngle = 0; // default
+    controls.maxPolarAngle = Math.PI; // default
+
+    loop.updatables.push(controls);
+    // loop.updatables.push(cube);
+
+    scene.add(cube, light);
+
+    const resizer = new Resizer(container, camera, renderer);
+  }
+
+  render() {
+    // draw a single frame
+    renderer.render(scene, camera);
+  }
+
+  start() {
+    loop.start();
+  }
+
+  stop() {
+    loop.stop();
+  }
+}
+
+export { World };
