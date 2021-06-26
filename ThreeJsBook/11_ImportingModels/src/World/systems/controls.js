1152b357d8ee65b845d2bfd0a614cc96d4d287b5
Evan Daley <evandaley@Evans-MacBook-Pro.local>
2021-06-25 19:46:26 -0700

    Small tweak

--- ThreeJsBook/11_ImportingModels/src/World/systems/controls.js
+++ ThreeJsBook/11_ImportingModels/src/World/systems/controls.js
@@ -6,7 +6,9 @@ function createControls(camera, canvas) {
   controls.enableDamping = true;
 
   // forward controls.update to our custom .tick method
-  controls.tick = () => controls.update();
+  controls.tick = () => {
+    controls.update()
+  }
 
   return controls;
 }
